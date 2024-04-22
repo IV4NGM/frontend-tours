b01c686b34fe3777d98a135c0da066815559d49c
IvanGM <ivangm_01@hotmail.com>
2024-04-22 01:09:08 -0600

    feat: Página búsqueda avanzada agregada

---
+++ frontend-tours/src/Pages/AdvancedSearch.jsx
@@ -0,0 +1,9 @@
+const AdvancedSearch = () => {
+  return (
+    <div className='page-container'>
+      <h2>Búsqueda avanzada</h2>
+    </div>
+  )
+}
+
+export default AdvancedSearch
--- frontend-tours/src/Routes/RoutesIndex.jsx
+++ frontend-tours/src/Routes/RoutesIndex.jsx
@@ -4,6 +4,7 @@ import Login from '@/Pages/Login'
 import TourInfo from '@/Pages/TourInfo'
 import PageNotFound from '@/Pages/PageNotFound'
 import About from '@/Pages/About'
+import AdvancedSearch from '@/Pages/AdvancedSearch'
 
 const RouterIndex = () => {
   return (
@@ -11,6 +12,7 @@ const RouterIndex = () => {
       <Route path='/' element={<Home />} />
       <Route path='/about-us' element={<About />} />
       <Route path='/tour/:id' element={<TourInfo />} />
+      <Route path='/search' element={<AdvancedSearch />} />
       <Route path='/login' element={<Login />} />
       <Route path='/*' element={<PageNotFound />} />
     </Routes>
